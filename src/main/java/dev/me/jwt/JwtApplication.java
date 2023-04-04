f244a125b8a9309c2291260816c867eabfe35884
josgro <josgrooten@gmail.com>
2023-04-04 20:51:43 +0200

    Initial commit

---
+++ src/main/java/dev/me/jwt/JwtApplication.java
@@ -0,0 +1,16 @@
+package dev.me.jwt;
+
+import dev.me.jwt.config.RsaKeyProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
